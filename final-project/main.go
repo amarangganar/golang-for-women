b1dec3f24d8b621edcb971a82891670ec9df1465
Rizkania Amaranggana <amarangganarizk@gmail.com>
2023-09-19 00:56:37 +0700

    remove godotenv.Load() on prod

--- final-project/main.go
+++ final-project/main.go
@@ -3,22 +3,16 @@ package main
 import (
 	"final_project/database"
 	"final_project/router"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/cloudinary/cloudinary-go/v2"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	var cld, err = cloudinary.New()
 	if err != nil {
-		log.Fatalf("Failed to intialize Cloudinary, %v", err)
+		log.Fatalf("failed to intialize Cloudinary, %v", err)
 	}
 
 	db, err := database.Init()
@@ -27,6 +21,4 @@ func main() {
 	}
 
 	router.Execute(db, cld).Run(os.Getenv("SERVER_PORT"))
-
-	fmt.Println("Hello world!")
 }
